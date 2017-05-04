32b320dad68f3baa5307f915e31dbd4af1d9a436
Anna Maltseva <anna.maltseva.work@gmail.com>
2017-05-04 17:12:12 +0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=students',
+    'username' => 'root',
+    'password' => '1q2w3e4r',
+    'charset' => 'utf8',
+];
