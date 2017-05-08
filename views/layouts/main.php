b02e88ac2f65afd6c6ce6c589c7255b1ba82f171
Anna Maltseva <anna.maltseva.work@gmail.com>
2017-05-08 10:13:20 +0300

    Layout header

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => '<img class="logo__img" src="/images/logo.png"  widht="250px" alt="Политехнический университет Петра Великого">',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
