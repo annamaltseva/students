6790c63e58f69f144a97197f759d66a82af26427
Anna Maltseva <anna.maltseva.work@gmail.com>
2017-05-26 13:34:18 +0300

    Migration for one more changes fix

--- migrations/m170526_102848_add_year_id_column_to_control_table.php
+++ migrations/m170526_102848_add_year_id_column_to_control_table.php
@@ -18,8 +18,6 @@ class m170526_102848_add_year_id_column_to_control_table extends Migration
     public function up()
     {
         $this->addColumn('control', 'year_id', $this->integer()->notNull());
-        $this->addColumn('control', 'group_id', $this->integer()->notNull());
-        $this->addColumn('control', 'subject_id', $this->integer()->notNull());
 
         // creates index for column `year_id`
         $this->createIndex(
@@ -38,39 +36,6 @@ class m170526_102848_add_year_id_column_to_control_table extends Migration
             'CASCADE'
         );
 
-        // creates index for column `group_id`
-        $this->createIndex(
-            'idx-control-group_id',
-            'control',
-            'group_id'
-        );
-
-        // add foreign key for table `group`
-        $this->addForeignKey(
-            'fk-control-group_id',
-            'control',
-            'group_id',
-            'group',
-            'id',
-            'CASCADE'
-        );
-
-        // creates index for column `subject_id`
-        $this->createIndex(
-            'idx-control-subject_id',
-            'control',
-            'subject_id'
-        );
-
-        // add foreign key for table `subject`
-        $this->addForeignKey(
-            'fk-control-subject_id',
-            'control',
-            'subject_id',
-            'subject',
-            'id',
-            'CASCADE'
-        );
     }
 
     /**
@@ -90,32 +55,6 @@ class m170526_102848_add_year_id_column_to_control_table extends Migration
             'control'
         );
 
-        // drops foreign key for table `group`
-        $this->dropForeignKey(
-            'fk-control-group_id',
-            'control'
-        );
-
-        // drops index for column `group_id`
-        $this->dropIndex(
-            'idx-control-group_id',
-            'control'
-        );
-
-        // drops foreign key for table `subject`
-        $this->dropForeignKey(
-            'fk-control-subject_id',
-            'control'
-        );
-
-        // drops index for column `subject_id`
-        $this->dropIndex(
-            'idx-control-subject_id',
-            'control'
-        );
-
         $this->dropColumn('control', 'year_id');
-        $this->dropColumn('control', 'group_id');
-        $this->dropColumn('control', 'subject_id');
     }
 }
